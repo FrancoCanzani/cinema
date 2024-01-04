fa85cef11a6f1e1e83957bf5a7cacd5217510d9a
Franco Canzani <francocanzani@gmail.com>
2024-01-04 21:44:11 +0100

    Adds table with movie schedule

--- app/components/slider.tsx
+++ app/components/slider.tsx
@@ -1,7 +1,5 @@
-'use client';
-
+import React from 'react';
 import { NowPlayingMovieResponse } from '../utils/types';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function MovieCarousel({
@@ -9,40 +7,89 @@ export default function MovieCarousel({
 }: {
   movies: NowPlayingMovieResponse[];
 }) {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const today = new Date();
+  const currentDayIndex = today.getDay();
+  const nextDays = Array.from(
+    { length: 3 },
+    (_, i) => (currentDayIndex + i) % 7
+  );
+
   return (
-    <div className='bg-gray-100 px-3 py-6 gap-y-4 flex flex-col'>
-      <nav className='flex items-center justify-between'>
-        <h2 className='font-semibold uppercase border-b-2 border-black w-fit'>
-          Current movies
-        </h2>
-        <Link href={'movies/all'} className='italic hover:underline text-sm'>
-          See all
-        </Link>
-      </nav>
-      <div className='flex carousel space-x-3'>
-        {movies.map((movie: NowPlayingMovieResponse) => (
-          <div key={movie.id} className='carousel-item flex flex-col'>
-            <Image
-              alt='Movie poster'
-              width={325}
-              height={400}
-              className='rounded-sm hover:scale-105 z-10 transition-all duration-200'
-              priority
-              src={`https://image.tmdb.org/t/p/w500${
-                movie.poster_path ?? movie.backdrop_path
-              }`}
-            />
-            <p className='py-2 h-12 text-sm font-semibold max-w-[90%]'>
-              {movie.title}
-            </p>
-            <Link
-              href={'#'}
-              className='text-orange-500 hover:underline font-semibold text-sm'
-            >
-              Buy Tickets
-            </Link>
-          </div>
-        ))}
+    <div className='relative'>
+      <div className='overflow-x-auto'>
+        <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 min-w-max'>
+          <thead className='text-xs border-b text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+            <tr>
+              <th
+                scope='col'
+                className='p-4 border-r-2 md:sticky md:left-0 bg-white'
+              >
+                Movie
+              </th>
+              {nextDays.map((dayIndex, index) => (
+                <th
+                  key={index}
+                  className={`${
+                    index === 0 && 'bg-gray-200 rounded-sm'
+                  } px-4 border py-2`}
+                >
+                  {daysOfWeek[currentDayIndex + index]}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie: NowPlayingMovieResponse) => (
+              <tr
+                key={movie.id}
+                className='even:bg-gray-100 border-b dark:bg-gray-800 dark:border-gray-700'
+              >
+                <th
+                  scope='row'
+                  className='px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white md:sticky md:left-0 bg-white'
+                >
+                  <Link
+                    href={`/movies/${movie.id}`}
+                    className='text-sm font-semibold hover:underline'
+                  >
+                    {movie.title}
+                  </Link>
+                </th>
+                {nextDays.map((dayIndex, index) => (
+                  <td key={index} className='px-4 py-2 border'>
+                    <div className='space-x-2'>
+                      <button className='bg-black rounded-sm px-2 py-1 text-white hover:text-gray-200'>
+                        17:15
+                      </button>
+                      <button className='bg-black rounded-sm px-2 py-1 text-white hover:text-gray-200'>
+                        20:00
+                      </button>
+                      {movie.popularity > 400 && (
+                        <button className='bg-black rounded-sm px-2 py-1 text-white hover:text-gray-200'>
+                          22:30
+                        </button>
+                      )}
+                      {movie.popularity > 700 && (
+                        <button className='bg-black rounded-sm px-2 py-1 text-white hover:text-gray-200'>
+                          00:15
+                        </button>
+                      )}{' '}
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
